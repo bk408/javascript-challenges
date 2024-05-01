016e9dda933783e5900aee871236832ac24b25d1
Bhavya Khatri <bhavyakhatri1912@gmail.com>
2024-05-01 23:34:56 +0530

    Approach explained in detail

--- Day 8/script.js
+++ Day 8/script.js
@@ -1,35 +1,39 @@
-
 // We have two approach two solve this issue
 
 // 1. Recursive method
 
 function factorial(n) {
-    if (n === 0) {
-        return 1;
-    } else {
-        return n * factorial(n - 1)
-    }
+  if (n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 }
+
+/*
+   Recursive method: It's like breaking down a big problem into smaller problems, solving each smaller problem the same way, 
+   and then putting the solutions together to solve the big problem.
+
+ */
 
 console.log(factorial(5));
 console.log(factorial(0));
 console.log(factorial(3));
 
-
 // 2. Iterative method
 
+/*
+ Iterative method in JavaScript refers to a process where a set of instructions is repeatedly executed until a specific condition is met.
+
+*/
 
 function factorial(n) {
-    let ans = 1;
+  let ans = 1;
 
-    if (n === 0) 
-        return 1;
+  if (n === 0) return 1;
 
-        for (let i = 2; i <= n; i++)
-            ans = ans * i;
-            return ans;
-        
-    
+  for (let i = 2; i <= n; i++) ans = ans * i;
+  return ans;
 }
 
 console.log(factorial(5));
